1f5e45d2a1c0672efed97a1e30d5716e48812a98
tanj299 <tanj299@gmail.com>
2019-06-02 15:50:34 -0400

    rest of prompt - fill/fillUncolored/clear and mouseover events on drag

--- pixelate-scripts.js
+++ pixelate-scripts.js
@@ -1,8 +1,19 @@
+// Jayson Tan | Raphael Vente 
+// Course: JS Bootcamp
+// Instructor(s): AJ Lapid / Justin Mintzer / Frank Rose 
+// Assignment: Assignment 4
+// References: Mozilla MDN ; Instructor's Notes 
+// NtS: Note to Self - learning outcomes for myself; instructors please disregard 
+
+
+/************************************** variables / mouse events **************************************/
 const table = document.getElementById("pixel-matrix");
+const colorSelect = document.getElementById("dropdownColors");
 let rows = 0; 
 let columns = 1; 
 let currentColor = "white" ;
-const colorSelect = document.getElementById("dropdownColors");
+let ondrag = true;
+
 
 // change color in dropdown menu
 // "change" is a JS event and fires when a new choice is made in a "select" element
@@ -10,7 +21,22 @@ colorSelect.addEventListener("change", () => {
 	currentColor = colorSelect.options[colorSelect.selectedIndex].value; 
 });
 
-function addRow () {
+
+// functions to check if mouse is being dragged 
+// assert: checks for dragging for drawing in grid
+document.body.onmousedown = function() {
+	ondrag = true; 
+}
+
+document.body.onmouseup = function() {
+	ondrag = false;
+}
+
+/**************************************  add/delete rows/columns functions **************************************/
+
+const addRow = () => {
+	currentColor = "white";
+
 	// append row at end of table 
 	let appendRow = table.insertRow(-1);
 
@@ -19,12 +45,14 @@ function addRow () {
 		newCell.style.backgroundColor = "white";
 
 		// changes color of cell 
+		// NtS: "mousedown" targets an element when a pointing device button is pressed 
 		newCell.addEventListener("mousedown", function(event) {
 			this.style.backgroundColor = currentColor; 
 		});
 
+		// NtS: mouse pointer comes over from an element 
 		newCell.addEventListener("mouseover", function(event) {
-			if(mousedown == true) {
+			if(mousedown === true) {
 				this.style.backgroundColor = currentColor; 
 			}
 		});
@@ -37,7 +65,7 @@ function addRow () {
 	rows++;
 }
 
-function deleteRow() {
+const deleteRow = () => {
 	// checks if row is less than 0 
 	if(rows < 0) {
 		rows = 0;
@@ -51,7 +79,8 @@ function deleteRow() {
 }
 
 
-function addCol() {
+const addCol = () => {
+	currentColor = "white";
 	// append column at end of table 
 	// assert: cells are added to each row 
 	for(let i = 0; i < rows; i++) {
@@ -64,9 +93,10 @@ function addCol() {
 		});
 
 		newCell.addEventListener("mouseover", function(event) {
-			// assert: mousedown = false; otherwise, color changes on hover 
+			// assert: ondrag = true; otherwise, color changes on hover 
 			// when column is added since it is the last event happening 
-			if(mousedown = false) { 
+			// also needed for dragging 
+			if(ondrag === true) { 
 				this.style.backgroundColor = currentColor; 
 			}
 		});
@@ -79,7 +109,7 @@ function addCol() {
 	columns++;
 }
 
-function deleteCol() {
+const deleteCol = () => {
 	// checks if column is less than 0 
 	if(columns < 0) {
 		columns = 0; 
@@ -95,6 +125,43 @@ function deleteCol() {
 	columns--;
 }
 
+/************************************** fill/fillUncolored/clear functions  **************************************/
 
+// fill all cells with current color
+const fillCells = () => {
+	for(let i = 0; i < rows; i++) {
+		for(let j = 0; j < columns; j++) {
+			let currentCell = table.rows[i].cells[j]; 
+			let cellColor = currentCell.style.backgroundColor;
+			currentCell.style.backgroundColor = currentColor;
+		}
+	}
+}
 
+// fill uncolored cells that is NOT default color with current color 
+const fillUncoloredCells = () => {
+	for(let i = 0; i < rows; i++) {
+		for(let j = 0; j < columns; j++) {
+			// assert: obtain current color of each cell 
+			let currentCell = table.rows[i].cells[j]; 
+			let cellColor = currentCell.style.backgroundColor; 
+
+			// checks if the current cell color is the default color [white]
+			// if it is, then it's the incorrect color; change to current color 
+			if(cellColor === "white") {
+				currentCell.style.backgroundColor = currentColor; 
+			}
+		}
+	}
+}
+
+// clear all cells color to default color 
+const clearAllCells = () => {
+	for(let i = 0; i < rows; i++) {
+		for(let j = 0; j < columns; j++) {
+			let currentCell = table.rows[i].cells[j]; 
+			currentCell.style.backgroundColor = "white";
+		}
+	}
+}
 
